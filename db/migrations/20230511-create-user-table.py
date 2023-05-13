dc02f5c72d0cf86f446a94253acb1cf0a582a2ca
Henry Speaker <73503466+HenrySpeaker@users.noreply.github.com>
2023-05-13 13:48:01 -0400

    implement migration to create user_account table

---
+++ db/migrations/20230511-create-user-table.py
@@ -0,0 +1,20 @@
+"""
+Create user table
+"""
+
+from yoyo import step
+
+__depends__ = {}
+
+steps = [
+    step(
+        """CREATE TABLE user_account (
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(50) UNIQUE NOT NULL, 
+            email VARCHAR(100) UNIQUE NOT NULL, 
+            password_hash VARCHAR(200) NOT NULL, 
+            notification_frequency SMALLINT CHECK (notification_frequency > 0 AND notification_frequency < 31), 
+            created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CLOCK_TIMESTAMP(), 
+            last_login TIMESTAMP WITH TIME ZONE DEFAULT CLOCK_TIMESTAMP())"""
+    )
+]
